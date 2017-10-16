c9ddc3067ad1c9e3d31fba7c57e3d9767cc15cc3
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-10-16 16:52:04 -0200

    Add files via upload

---
+++ reprogramafy-master/script.js
@@ -0,0 +1,12 @@
+var botao = document.getElementById("botao");
+var modal = document.querySelector(".bloco");
+var fundo = document.getElementById("fundo");
+
+botao.onclick = function() {
+    modal.classList.add("visivel");
+    fundo.classList.add("visivel");
+}
+fecha.onclick = function() {
+    modal.classList.remove("visivel");
+    fundo.classList.remove("visivel");
+}
