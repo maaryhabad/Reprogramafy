92f5b1a27c91342e76fedfe237997d85fc1901aa
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-10-17 21:52:01 -0200

    novo

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-var botao = getElementById("botao");
+var botao = document.getElementById("botao");
+// var modal = document.querySelector(".bloco");
+var fundo = document.getElementById("fundo");
 
-console.log(botao);
+botao.onclick = function() {
+    fundo.classList.add("visivel"); /*a modal não precisa ser mostrada porque está dentro do fundo usando display block
+*/}
+fecha.onclick = function() {
+    fundo.classList.remove("visivel");
+}
+
+fundo.onclick = function(e) { /*e é um parâmetro*/  
+    if (e.target == fundo) {
+        fundo.classList.remove("visivel");
+    }
+}
