2773e03e7655c2e47c836de8949764208bdc0bb7
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-10-20 11:56:07 -0200

    modais

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-var login = document.getElementById("login");
-var cadastro = document.getElementById("cadastro");
-
-var modalLogin = document.querySelector(".blocoLogin");
 var fundoLogin = document.getElementById("fundoLogin");
 var fecharLogin = document.getElementById("fecharLogin");
 
-var modalCadastro = document.querySelector(".blocoCadastro");
 var fundoCadastro = document.getElementById("fundoCadastro");
 var fecharCadastro = document.getElementById("fecharCadastro");
 // var usuario;
@@ -14,70 +9,40 @@ var fecharCadastro = document.getElementById("fecharCadastro");
 var formulario = document.querySelector('form');
 
 login.onclick = function() {
-    modalLogin.classList.add("visivel");
     fundoLogin.classList.add("visivel");
-
-    formulario.onsubmit = function() {
-        if(email.value !== 'admin@admin.com' ||
-            senha.value !== 'admin') {
-            alert('Email errado');
-            modal.classList.add('erro');
-            
-            setTimeout(function() {
-                modal.classList.remove('erro');
-                }, 1000);
-            return false;
-        }
-
-        localStorage.usuario = email.value;
-    }
-console.log(email);
 }
 
-cadastro.onclick = function() {
-    modalCadastro.classList.add("visivel");
-    fundoCadastro.classList.add("visivel");
-
-    formulario.onsubmit = function() {
-        if(email.value !== 'admin@admin.com' ||
-            senha.value !== 'admin') {
-            alert('Email errado');
-            modal.classList.add('erro');
-            
-            setTimeout(function() {
-                modal.classList.remove('erro');
-                }, 1000);
-            return false;
-        }
-
-        localStorage.usuario = email.value;
-    }
-    console.log(email);
-}
-
-fecharLogin.onclick = function() {
-    modalLogin.classList.remove("visivel");  
-    fundoLogin.classList.remove("visivel");  
-}
-fecharCadastro.onclick = function() {
-    modalCadastro.classList.remove("visivel");
-    fundoCadastro.classList.remove("visivel");
-}
 fundoLogin.onclick = function(e) {
     console.log(e);
     if (e.target == fundoLogin) {
-        modalLogin.classList.remove("visivel");
         fundoLogin.classList.remove("visivel");
-    }
-    
+    }    
 }
+
+fecharLogin.onclick = function() {
+    fundoLogin.classList.remove("visivel");  
+}
+
+
+
+
+
+
+cadastro.onclick = function() {
+    fundoCadastro.classList.add("visivel");
+}
+
 fundoCadastro.onclick = function(e) {
     console.log(e);
     if (e.target == fundoCadastro) {
-        modalCadastro.classList.remove("visivel");
         fundoCadastro.classList.remove("visivel");
-    }
-    
+    }   
 }
+
+fecharCadastro.onclick = function() {
+    fundoCadastro.classList.remove("visivel");
+}
+
+
 // Login
 
